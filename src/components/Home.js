8b0636151462576a2fd7b8dd564f976a4d6b794e
Simon Koerner <simon.koerner@nexenio.com>
2022-07-18 23:39:33 +0200

    Add simple error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,37 +7,43 @@ function Home() {
         setBlogs(blogs.filter(blog => blog.id !== idOfBlogToDelete))
       }
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState([])
 
     const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
             fetch('http://localhost:7777/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsLoading(false)
-            })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("Error fetching data for that resource")
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setError(null)
+                    setBlogs(data)
+                    setIsLoading(false)
+                })
+                .catch(err => {
+                    setIsLoading(false)
+                    setError(err.message)
+                })
     }, [])
 
   return (
     <div className="home">
+        {error && <div>{error}</div>}
         {!isLoading ?
         <>
-        {blogs && <>
+        {blogs.length > 0 ? <>
         <BlogList blogs={blogs} 
                   title={"All blogs"} 
                   handleDelete={handleDelete}/>
         <BlogList blogs={blogs.filter(entry => entry.author.toLowerCase() === 'mario')} 
                   title={"Mario's blogs"}
                   handleDelete={handleDelete}/>
-        </>}
+        </> : ''}
         </>
         : <div>Loading...</div>}
     </div>
