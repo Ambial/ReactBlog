f9e872a421c24f66e747e8892deab5e41c0c83ee
Simon Koerner <simon.koerner@nexenio.com>
2022-07-18 17:53:01 +0200

    Add Blogentry component and dummy list

---
+++ src/components/BlogEntry.js
@@ -0,0 +1,10 @@
+function BlogEntry({blog}) {
+  return (
+    <div className="blog-preview">
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+    </div>
+  )
+}
+
+export default BlogEntry
