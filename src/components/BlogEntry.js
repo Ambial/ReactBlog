29dcea163a93d12774a127d51a6a6543497a3524
Simon Koerner <simon.koerner@nexenio.com>
2022-07-19 16:32:39 +0200

    Navigate from list entry to detailsview using Link

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
+
 function BlogEntry({blog}) {
   return (
     <div className="blog-preview">
+      <Link to={`blogs/${blog.id}`} >
         <h2>{blog.title}</h2>
         <p>Written by {blog.author}</p>
+      </Link>
     </div>
   )
 }
--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom"
+
 function BlogDetails() {
+  const { id } = useParams()
+
   return (
     <div className="blog-details">
-        <h2>BlogDetails</h2>
+        <h2>BlogDetails for the blog with the id {id}</h2>
     </div>
   )
 }
