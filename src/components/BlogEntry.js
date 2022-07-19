d4bbe83fa7160c7bc34ee2570f4a82c9306c8be6
Simon Koerner <simon.koerner@nexenio.com>
2022-07-19 16:34:52 +0200

    Change Link to useNavigate

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function BlogEntry({blog}) {
+  const navigate = useNavigate()
+
   return (
-    <div className="blog-preview">
-      <Link to={`blogs/${blog.id}`} >
+    <div className="blog-preview"
+         onClick={() => navigate(`blogs/${blog.id}`)}>
         <h2>{blog.title}</h2>
         <p>Written by {blog.author}</p>
-      </Link>
     </div>
   )
 }
