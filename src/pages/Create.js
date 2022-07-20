fbfc4c1d6d6dad574348dc7e4268e673521af605
Simon Koerner <simon.koerner@nexenio.com>
2022-07-20 16:55:16 +0200

    Add simple create blog form, no function yet

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,23 @@
 function Create() {
   return (
-    <div>Create</div>
+    <div className="create">
+      <h2>Add a new blog entry</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text"
+               required
+        ></input>
+        <label>Blog body:</label>
+        <textarea required></textarea>
+        <label>Blog author</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="luigi">luigi</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+    </div>
   )
 }
 
