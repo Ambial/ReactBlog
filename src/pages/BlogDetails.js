1d2d57d9fc2f702f3bced090058c2391ec18797a
Simon Koerner <simon.koerner@nexenio.com>
2022-07-31 20:43:01 +0200

    Add delete button and functionality

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../customHooks/useFetch"
 import { BASE_URL, BLOGS_PATH } from "../utils/consts"
+import { useNavigate } from "react-router-dom"
 
 function BlogDetails() {
   const { id } = useParams()
   const { data:blog, isLoading, error} = useFetch(`${BASE_URL}${BLOGS_PATH}/${id}`)
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+    fetch(`${BASE_URL}${BLOGS_PATH}/${id}`, {
+      method: 'DELETE'
+    }).then(() => {
+      navigate('/')
+    })
+  }
 
   return (
     <div className="blog-details">
@@ -15,6 +25,7 @@ function BlogDetails() {
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
             <div>{blog.body}</div>
+            <button onClick={handleClick}>Delete</button>
         </article>
         )}
     </div>
