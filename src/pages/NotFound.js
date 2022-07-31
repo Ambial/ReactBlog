201beedbe5e0b8b18c12f0a9fadb03b06f0664e4
Simon Koerner <simon.koerner@nexenio.com>
2022-07-31 20:54:31 +0200

    Optimize NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 function NotFound() {
   return (
-    <div>The page you're looking for is in another castle...</div>
+    <div className="not-found">
+      <h2>Sorry</h2>      
+      <p>The page you're looking for is in another castle...</p>
+      <Link to="/">Back to the homepage...</Link>
+    </div>
   )
 }
 
